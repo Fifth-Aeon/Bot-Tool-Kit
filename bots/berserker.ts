3606b830b87d8c2620302559b963da7a7333482b
William Ritson <william.ritson@gmail.com>
2018-10-12 12:33:59 -0700

    Added Simple Bot for testing.

---
+++ bots/berserker.ts
@@ -0,0 +1,25 @@
+import { DefaultAI } from '../CCG-Model/ai/defaultAi';
+
+/**
+ * Berserker is a version of the default A.I that always makes every available attack and never blocks.
+ * 
+ * As a consequence this A.I is unlikely to be very competent.
+ * 
+ * Otherwise its identical to DefaultAI
+ */
+export class BerserkerAI extends DefaultAI {
+
+    /** Attack with all legal units */
+    protected attack() {
+        let potentialAttackers = this.game.getBoard()
+            .getPlayerUnits(this.playerNumber)
+            .filter(unit => unit.canAttack());
+
+        for (let attacker of potentialAttackers) {    
+            this.game.declareAttacker(attacker);
+        }
+    }
+
+    /** Never block anything */
+    protected block() {}
+}
