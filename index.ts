0c5b55202eab7aed91a7e6b190983b9278b252b3
William Ritson <william.ritson@gmail.com>
2018-10-12 12:34:31 -0700

    Added basic code to invoke the Typescript compiler on bots.

--- index.ts
+++ index.ts
@@ -1,1 +1,48 @@
-console.log('Hello world!');
+import * as fs from "fs";
+import * as util from "util";
+import * as ts from "typescript";
+
+function compile(fileNames: string[], options: ts.CompilerOptions): void {
+  let program = ts.createProgram(fileNames, options);
+  let emitResult = program.emit();
+
+  let allDiagnostics = ts
+    .getPreEmitDiagnostics(program)
+    .concat(emitResult.diagnostics);
+
+  allDiagnostics.forEach(diagnostic => {
+    if (diagnostic.file) {
+      let { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
+        diagnostic.start!
+      );
+      let message = ts.flattenDiagnosticMessageText(
+        diagnostic.messageText,
+        "\n"
+      );
+      console.log(
+        `${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`
+      );
+    } else {
+      console.log(
+        `${ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n")}`
+      );
+    }
+  });
+
+  let exitCode = emitResult.emitSkipped ? 1 : 0;
+  console.log(`Process exiting with code '${exitCode}'.`);
+  process.exit(exitCode);
+}
+
+const readdir = util.promisify(fs.readdir);
+
+(async () => {
+  let files = await readdir("bots");
+  let botFiles = files.filter(name => name.includes(".ts")).map(name => "bots/" + name);
+  compile(botFiles, {
+    noEmitOnError: true,
+    noImplicitAny: true,
+    target: ts.ScriptTarget.ES2017,
+    module: ts.ModuleKind.CommonJS
+  });
+})();
